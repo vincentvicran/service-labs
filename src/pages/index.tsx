1caaca0d04ef44565705dfa0845b2f2639e4f3dc
vincentvicran <vvicran@gmail.com>
2022-02-28 20:16:49 +0545

    Testing Dark Theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,40 @@
 /* eslint-disable react/jsx-key */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'components/Container';
 import { ModalProvider } from 'styled-react-modal';
 import Sidebar from 'components/Sidebar';
 import { Props } from 'utils/typings';
 import useSWR from 'swr';
 import { fetcher, swrOptions } from 'utils/swrFetcher';
+import { ThemeProvider } from 'next-themes';
+import { WbSunnyRounded, ShieldMoonRounded } from '@mui/icons-material';
+import { useTheme } from 'next-themes';
 
 const Home = () => {
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    const { systemTheme, theme, setTheme } = useTheme('dark');
+
+    const renderThemeChanger = () => {
+        if (!mounted) return null;
+        const currentTheme = theme === 'system' ? systemTheme : theme;
+
+        return currentTheme === 'dark' ? (
+            <WbSunnyRounded
+                className="w-7 h-7"
+                role="button"
+                onClick={() => setTheme('light')}
+            />
+        ) : (
+            <ShieldMoonRounded
+                className="w-7 h-7"
+                role="button"
+                onClick={() => setTheme('dark')}
+            />
+        );
+    };
     const { data, error } = useSWR('coins', fetcher, swrOptions);
 
     if (data) console.log(data);
@@ -18,16 +45,19 @@ const Home = () => {
 
     return (
         <div>
-            <ModalProvider>
-                <div className="flex flex-1 w-screen h-screen">
-                    <div className="flex shrink  w-px-300">
-                        <Sidebar />
+            <ThemeProvider enableSystem={true} attribute="class">
+                <ModalProvider>
+                    <div className="flex flex-1 w-screen h-screen dark:bg-secondary-800">
+                        <div className="flex shrink  w-px-300">
+                            <Sidebar />
+                            {renderThemeChanger()}
+                        </div>
+                        <div className="flex flex-1">
+                            <Container data={data} />
+                        </div>
                     </div>
-                    <div className="flex flex-1">
-                        <Container data={data} />
-                    </div>
-                </div>
-            </ModalProvider>
+                </ModalProvider>
+            </ThemeProvider>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     mode: 'jit',
+    darkMode: 'class',
     content: ['./src/**/*.{ts,tsx,js,jsx,css}'],
     theme: {
         colors: {
+            ...defaultTheme.colors,
             primary: colors.orange,
             secondary: colors.gray,
             info: colors.cyan,
